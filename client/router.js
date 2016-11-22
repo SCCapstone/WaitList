6d91bd752c074ba114d7e53dfae646fdfdf39b8b
Eddie Pace <epace94@gmail.com>
2016-11-21 22:21:15 -0500

    added in some pages
    Admin page, login page and arranged the flies a little.

--- client/router.js
+++
@@ -1,9 +0,0 @@
-if(Meteor.isClient) {
-  Router.route('/', function() {
-    this.render('home');
-  }),
-
-  Router.route('/test', function() {
-    this.render('test');
-  });
-}
---
+++ router/router.js
@@ -0,0 +1,13 @@
+if(Meteor.isClient) {
+  Router.route('/', function() {
+    this.render('home');
+  }),
+
+  Router.route('/login', function() {
+    this.render('login');
+  }),
+  
+  Router.route('/adminPage', function() {
+    this.render('adminPage');
+  });
+}
