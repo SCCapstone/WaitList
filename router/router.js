6dc1b536d8565d39dbb7f730f655616850ae6691
redmonc2 <[redmonc2@email.sc.edu]>
2016-11-28 18:03:00 -0500

    FRESH

--- router/router.js
+++ router/router.js
@@ -1,12 +1,13 @@
 if(Meteor.isClient) {
-  Router.route('/', function() {
+  Router.route('/', function () {
     this.render('home');
   }),
 
-  Router.route('/login', function() {
-    this.render('login');
-  }),
-  
-  Router.route('/adminPage', function() {
-    this.render('adminPage');
-  });
+      Router.route('/login', function () {
+        this.render('login');
+      }),
+
+      Router.route('/adminPage', function () {
+        this.render('adminPage');
+      });
+}
