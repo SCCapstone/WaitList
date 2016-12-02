1811b5df7363726ff6f805fb0a875de806e78676
redmonc2 <redmonc2@email.sc.edu>
2016-12-02 16:46:25 -0500

    testing

--- router/router.js
+++ router/router.js
@@ -2,11 +2,6 @@ if(Meteor.isClient) {
   Router.route('/', function () {
     this.render('home');
   }),
-
-      Router.route('/login', function () {
-        this.render('login');
-      }),
-
       Router.route('/adminPage', function () {
         this.render('adminPage');
       });
