483b9ea1c3a4c6c8803294f27c632adf8229f7e4
Ryan Morvay <morvayr@email.sc.edu>
2017-03-01 10:45:09 -0500

    new alert after submitting form

--- client/SignInPage/SignInPage.js
+++ client/SignInPage/SignInPage.js
@@ -1,9 +1,12 @@
 import '../../imports/ui/body.js';
 
+
 if(Meteor.isClient){
     Template.home.events({
-        "submit #student-form": function(){
+        "submit #student-form": function() {
             Meteor.call("sendSMS");
+            // alert("You have been added to the WaitList");
+            swal("Success!", "You have been added to the WaitList!", "success")
         }
-    })
-}
+})
+};
