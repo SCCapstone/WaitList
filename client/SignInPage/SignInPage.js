9d171c0f5e3f73c1187b36a7485a8b769b928ce0
Eddie Pace <epace94@gmail.com>
2017-03-03 16:18:49 -0500

    added send sms stuff back in

--- client/SignInPage/SignInPage.js
+++ client/SignInPage/SignInPage.js
@@ -1,18 +1,25 @@
 import '../../imports/ui/body.js';
-import '../../imports/api/students.js';
+import '../../imports/api/students';
 
 if(Meteor.isClient){
     
     Template.home.events({
         "submit #student-form": function(event) {
             event.preventDefault();
-            var phoneNumber = '+18.....';
-            Meteor.call("sendSMS", phoneNumber);
-            // alert("You have been added to the WaitList");
-            
+            var textService = Students.findOne({},{sort:{createdAt:-1},limit:1, fields:{Disclaimer:1, _id:0}}).Disclaimer;
+            var phoneNumber = Students.findOne({},{sort:{createdAt:-1},limit:1, fields:{PhoneNumber:1, _id:0}}).PhoneNumber;
+            phoneNumber = "+1" + phoneNumber;
+            if(textService == true) {
+                Meteor.call("sendSMS",phoneNumber);
+            }
+            console.log(phoneNumber);
+            console.log(textService);
             swal("Success!", "You have been added to the WaitList", "success")
         }
     });
+    /*Template.home.twilioPhoneNumber = function(){
+        var phoneNumber = '+18434124314'
+    };*/
 };
 
  Template.home.count = function() {
