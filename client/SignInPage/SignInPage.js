67318b99a7615fcdf516ecee75b1229215e1f643
Eddie Pace <epace94@gmail.com>
2017-03-22 09:59:21 -0400

    Took out create account button.
    Also changed event for "check-in" button to single click instead of
    double click.

--- client/SignInPage/SignInPage.js
+++ client/SignInPage/SignInPage.js
@@ -1,31 +1,6 @@
 import '../../imports/ui/body.js';
 import '../../imports/api/students';
 
-/*if(Meteor.isClient){
-    
-    Template.home.events({
-        "submit #studentForm": function(event) {
-            event.preventDefault();
-            var textService = Students.findOne({},{sort:{createdAt:-1},limit:1, fields:{Disclaimer:1, _id:0}}).Disclaimer;
-            var phoneNumber = Students.findOne({},{sort:{createdAt:-1},limit:1, fields:{PhoneNumber:1, _id:0}}).PhoneNumber;
-            phoneNumber = "+1" + phoneNumber;
-            var totalCount = Students.find({countNumber:1}).count();
-            var waitTime = "Approximate Wait Time: " + totalCount*15 +" Minutes";
-            if(textService == true) {
-                Meteor.call("sendSMS",phoneNumber);
-            }
-            document.getElementById("insert").innerHTML = waitTime;
-            console.log(phoneNumber);
-            console.log(textService);
-            console.log(totalCount);
-
-        }
-    });
-    /*Template.home.twilioPhoneNumber = function(){
-        var phoneNumber = '+18434124314'
-    };
-};*/
-
 Template.home.onCreated(function() {//This is not needed,only for testing purposes to access databse from sign-in page
     Meteor.subscribe("allStudents");
 });
