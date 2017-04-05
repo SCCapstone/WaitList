99817f42f7cad4b185e0551e6bebb09f09d2c4db
Eddie Pace <epace94@gmail.com>
2017-04-05 09:58:31 -0400

    Added wait time on main screen
    Added wait time on main screen and added in prompt for user to get their
    specific wait time.

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -10,3 +10,27 @@ Accounts.createUser({
         email: 'admin@email.com',
         password: 'asdfasdf',
 });
+
+Template.checkWaitTime.events({
+    'click .checkTime' (){
+        var timeCheck = prompt("Please enter your phone number below");
+        var phoneNum = Students.findOne({PhoneNumber:timeCheck});
+        console.log(timeCheck);
+        console.log(phoneNum);
+        //still working on this to show individual's wait time
+        //we can use the rank attribute and just multiply that by 15 to get wait time
+        //var total = Students.findOne({},{fields:{PhoneNumber:timeCheck}}).rank;
+
+        //this one does not show individual wait time but total wait time
+        var waitTime = Students.findOne({PhoneNumber:timeCheck}).waitTime;
+        console.log(waitTime);
+        if (timeCheck != null && phoneNum != null) {
+           swal("Your approximate wait time is " + waitTime + " minutes");
+        }
+        else {
+            swal("Something went wrong", "Your phone number was not found on the wait list, " +
+                 "make sure you entered your number correctly and have filled out the " +
+                 "form below.", "error");
+        }
+    }
+});
