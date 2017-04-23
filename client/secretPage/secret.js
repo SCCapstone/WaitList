6edf551303bd0b754859ad6c5f59cae5f7000740
liangrunquan <rliang@email.sc.edu>
2017-04-23 17:13:07 -0400

    Add files via upload

--- client/secretPage/secret.js
+++ client/secretPage/secret.js
@@ -1,8 +1,23 @@
 import '../../imports/ui/body.js';
 import '../../imports/api/students';
 
+Template.secret.onCreated (function() {
+    this.subscribe("allStudents");
+});
 Template.secret.helpers({
-    secretTime: function() {
-        Students.findOne({PhoneNumber: "1111111111"}).waitTime;
+    waitTime: function() {
+        var current = document.URL;
+        current = current.substr(22);
+        console.log(current);
+        var phoneNum = Students.findOne({PhoneNumber:current});
+        console.log(phoneNum);
+        if(phoneNum != null && current != null){
+            var time1 = Students.findOne({PhoneNumber:current}).waitTime;
+            return time1 + "minutes";
+        }
+        else{
+            return "You are not in the list";
+        }
     }
 });
+
