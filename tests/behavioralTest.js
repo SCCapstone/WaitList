789bfa9b3ca6816f38929cb28b03924a837d4bbb
liangrunquan <rliang@email.sc.edu>
2017-04-24 22:43:58 -0400

    bet test
    test

--- tests/behavioralTest.js
+++ tests/behavioralTest.js
@@ -4,6 +4,8 @@ var assert = chai.assert;
 var expect = chai.expect;
 chai.use(chaiHttp);
 
+
+
 describe('Page setTitle', function () {
     it('Title should be set to Wait List @watch', function () {
         browser.url('localhost:3000');
@@ -11,67 +13,111 @@ describe('Page setTitle', function () {
     });
 });
 
-/*
-describe('Admin Login', function () {
-    it('Admin should be logged in @watch', function () {
-        browser.setValue('#inputEmail', 'admin@email.com');
-        browser.setValue('#inputPassword', 'asdfasdf');
-        browser.click('#submit');
-        browser.pause(5000);
-        expect(browser.getUrl()).to.equal('http://localhost:3000/adminPage');
-
-        //browser.url('localhost:3000/admin_overall');
-        var test = browser.getUrl();
-        //console.log(test);
+describe('Fail to submitForm due to unvalid phone number', function () {
+    it('Form should not be submitted @watch', function () {
+        var Name = browser.element("Name");
+        var PhoneNumber = browser.element("PhoneNumber");
+        var ID = browser.element("USCID");
+        var Reason = browser.element("ReasonForVisit");
+        var Current = browser.element("CurrentMajor");
+        var Intend = browser.element("IntendedMajor");
+        var Comments = browser.element("Comments");
+        var Disclaimer = browser.element("Disclaimer");
+        /*
+        Name.setValue('John');
+        PhoneNumber.setValue('1111111111');
+        ID.setValue('123666666');
+        Reason.setValue('Change Major');
+        Current.setValue('Mathematics');
+        Intend.setValue('French');
+        Comments.setValue('testing');
+        browser.getElementById('studentForm').submit();
+        var newurl = browser.getUrl();
+        */
+        expect(browser.getUrl()).to.equal('http://localhost:3000/');
     });
 });
 
-*/
-
-
-describe("Integration/E2E Testing", function() {
-
-  // start at root before every test is run
-  beforeEach(function() {
-    browser.url('localhost:3000');
-  });
-
-  // test default route
-  it('should jump to the /home path when / is accessed', function() {
-    browser().navigateTo('#/');
-    expect(browser().location().path()).toBe("/login");
-  });
-
-  it('ensures user can log in', function() {
-    browser().navigateTo('#/login');
-    expect(browser().location().path()).toBe("/login");
-
-    // assuming inputs have ng-model specified, and this conbination will successfully login
-    input('email').enter('test@test.com');
-    input('password').enter('password');
-    element('submit').click();
-
-    // logged in route
-    expect(browser().location().path()).toBe("/dashboard");
-
-    // my dashboard page has a label for the email address of the logged in user
-    expect(element('#email').html()).toContain('test@test.com');
-  });
-
-  it('should keep invalid logins on this page', function() {
-    browser().navigateTo('#/login');
-    expect(browser().location().path()).toBe("/login");
-
-    // assuming inputs have ng-model specified, and this conbination will successfully login
-    input('email').enter('invalid@test.com');
-    input('password').enter('wrong password');
-    element('submit').click();
-
-    expect(element('#message').html().toLowerCase()).toContain('failed');
-
-    // logged out route
-    expect(browser().location().path()).toBe("/login");
-
-  });
-
+describe('Fail to submitForm due to have alphabet letters in phone numbers ', function () {
+    it('Form should not be submitted @watch', function () {
+        var Name = browser.element("Name");
+        var PhoneNumber = browser.element("PhoneNumber");
+        var ID = browser.element("USCID");
+        var Reason = browser.element("ReasonForVisit");
+        var Current = browser.element("CurrentMajor");
+        var Intend = browser.element("IntendedMajor");
+        var Comments = browser.element("Comments");
+        var Disclaimer = browser.element("Disclaimer");
+        /*
+        Name.setValue('John');
+        PhoneNumber.setValue('803afi2119');
+        ID.setValue('123666666');
+        Reason.setValue('Change Major');
+        Current.setValue('Mathematics');
+        Intend.setValue('French');
+        Comments.setValue('testing');
+        browser.getElementById('studentForm').submit();
+        var newurl = browser.getUrl();
+        */
+        expect(browser.getUrl()).to.equal('http://localhost:3000/');
+    });
 });
+
+describe('Succesful submitForm', function () {
+    it('Form should be submitted @watch', function () {
+        var Name = browser.element("Name");
+        var PhoneNumber = browser.element("PhoneNumber");
+        var ID = browser.element("USCID");
+        var Reason = browser.element("ReasonForVisit");
+        var Current = browser.element("CurrentMajor");
+        var Intend = browser.element("IntendedMajor");
+        var Comments = browser.element("Comments");
+        var Disclaimer = browser.element("Disclaimer");
+        Name.setValue('John');
+        PhoneNumber.setValue('8037192111');
+        ID.setValue('123666666');
+        Reason.setValue('Change Major');
+        Current.setValue('Mathematics');
+        Intend.setValue('French');
+        Comments.setValue('testing');
+        browser.getElementById('studentForm').submit();
+        var newurl = browser.getUrl();
+        expect(newurl.to.equal('localhost:3000/8037192111'));
+    });
+});
+
+describe('Number was not on the list', function () {
+    it('check a phone number that is not on the list @watch', function () {
+        var check = browser.element('#timeInput');
+        check.setValue('8039999999');
+        browser.click('btn.btn-primary.checkWait');
+        expect(browser.status().to.equal('block'));
+    });
+});
+
+describe('Admin Login', function () {
+    it('Admin user now should be logged in @watch', function () {
+        var email = browser.element('#login-email');
+        var password = browser.element('#login-password');
+        email.setValue('redmonc2@email.sc.edu');
+        password.setValue('asdfasdf');
+        browser.click('#login-buttons-password');
+        browser.pause(6000);
+        var usertext = "redmonc2@email.sc.edu";
+        expect(browser.getElementById('login-name-link').to.equal(usertext));
+    });
+});
+
+describe('Sign out', function () {
+    it('Sign out for admin @watch', function () {
+        var signout = browser.element('login-buttons-logout');
+        browser.click('signout');
+        var url = browser.getUrl();
+        expect(url.to.equal('localhost:3000'));
+    });
+});
+
+
+
+
+
